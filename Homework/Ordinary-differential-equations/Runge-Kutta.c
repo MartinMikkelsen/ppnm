ce2312db07a7b0213b1359f8ea88e994da07c5ae
Martin Mikkelsen <MartinMikkelsen@post.au.dk>
2021-06-20 14:56:24 +0200

    SIR models

--- Homework/Ordinary-differential-equations/Runge-Kutta.c
+++ Homework/Ordinary-differential-equations/Runge-Kutta.c
@@ -8,7 +8,7 @@
 #define FOR(i) for(int i=0;i<n;i++)
 
 
-//Implement an embedded Runge-Kutta stepper, rkstepXY -- where XY is the imbedded method. The function must estimate the values y_k(t+h) and store them in a vector* yh, estimate the errors delta-y_k and store in a vector err. Using the same approach as in the book but with gsl. 
+//Implement an embedded Runge-Kutta stepper, rkstepXY -- where XY is the imbedded method. The function must estimate the values y_k(t+h) and store them in a vector* yh, estimate the errors delta-y_k and store in a vector err. Using the same approach as in the book but with gsl.
 
 
 void rkstep12(void f(double x, gsl_vector* y, gsl_vector* dydx)
@@ -26,7 +26,6 @@ void rkstep12(void f(double x, gsl_vector* y, gsl_vector* dydx)
         double yti = yxi + h / 2 * k0i;
         gsl_vector_set(yt, i, yti);
     }
-    //Calculate second order
     f(x + h / 2, yt, k1);
     for (int i = 0; i < n; i++) {
         double yxi = gsl_vector_get(yx, i);
@@ -34,7 +33,6 @@ void rkstep12(void f(double x, gsl_vector* y, gsl_vector* dydx)
         double yhi = yxi + h * k1i;
         gsl_vector_set(yh, i, yhi);
     }
-    //Error estimate
     for (int i = 0; i < n; i++) {
         double k0i = gsl_vector_get(k0, i);
         double k1i = gsl_vector_get(k1, i);
@@ -48,68 +46,64 @@ void rkstep12(void f(double x, gsl_vector* y, gsl_vector* dydx)
 }
 
 void rkstep23(void (*f)(int n, double x, double* y, double* dydx), int n, double x, double* y_curr, double h, double* y_next, double* err) {
-   
-   // declare variables
+
    double k1[n];
    double k2[n];
    double k3[n];
    double k4[n];
    double y_temp[n];
 
-   // first point
+   //first order
    f(n, x, y_curr, k1);
    for (int i = 0; i < n; ++i) {
       y_temp[i] = y_curr[i] + (1.0/2)*k1[i]*h;
    }
 
-   // second point
+   //second order
    f(n, x + (1.0/2)*h, y_temp, k2);
    for (int i = 0; i < n; ++i) {
       y_temp[i] = y_curr[i] + (3.0/4)*k2[i]*h;
    }
 
-   // third point and second order estimate of y(x + h)
+   //third order
    f(n, x + (3.0/4)*h, y_temp, k3);
    for (int i = 0; i < n; ++i) {
       y_temp[i] = y_curr[i] + ((2.0/9)*k1[i] + (1.0/3)*k2[i] + (4.0/9)*k3[i])*h;
    }
 
-   // fourth point, third order estimate of y(x + h), and error estimate
    f(n, x + h, y_temp, k4);
    for (int i = 0; i < n; ++i) {
       y_next[i] = y_curr[i] + ((7.0/24)*k1[i] + (1./4)*k2[i] + (1.0/3)*k3[i] + (1.0/8)*k4[i])*h;
       err[i] = y_next[i] - y_temp[i];
    }
-  
+
 
 }
 
 //Implement an adaptive-step-size driver routine.
 //
 int driver(
-	void (*f)(int n, double x, double* y, double* dydx), // right-hand-side of dy/dx = f(x, y) 
-    int n,          // size of vectors 
-	double  a,      // the start-point a 
-	double  b,      // the end-point of the integration 
-	double* ya,     // y(a) 
-	double* yb,     // y(b) to be calculated 
-	double  h,      // initial step-size 
-	double  acc,    // absolute accuracy goal 
-	double  eps,    // relative accuracy goal 
-   char*   outfile // trajectory file
-) { 
+	void (*f)(int n, double x, double* y, double* dydx), // right-hand-side of dy/dx = f(x, y)
+    int n,          // size of vectors
+	double  a,      // the start-point a
+	double  b,      // the end-point of the integration
+	double* ya,     // y(a)
+	double* yb,     // y(b) to be calculated
+	double  h,      // initial step-size
+	double  acc,    // absolute accuracy goal
+	double  eps,    // relative accuracy goal
+    char*   outfile // target file
+) {
 
-   // declare variables
    int k = 0;     // step counter
    double x;      // current x
    double y[n];   // current y vector
    double yh[n];  // estimate of y(x + h)
    double err[n]; // error estimate vector
-   double sum;    // intermediate variable
-   double tau;    // local tolerance
+   double s;    // intermediate variable
+   double tol;    // local tolerance
    FILE* file = fopen(outfile, "w");
 
-   // prepare first step and write it to file
    x = a;
    fprintf(file, "%20.12e ", x);
    for (int i = 0; i < n; ++i) {
@@ -119,84 +113,51 @@ int driver(
    fprintf(file, "\n");
 
 
-   // loop until endpoint is reached
-   while (x < b) {
-      //printf("===============\n");
-      //printf("Step no. %3d\n", k);
-      //printf("x = %.4f\ny =\n", x);
-      //cvector_print(y, n);
-
-      // avoid overstepping
-      if (x + h > b) {
+   while (x < b)
+   {
+      if (x + h > b)
+      {
          h = b - x;
       }
 
-      // make step
-      rkstep23(f, n, x, y, h, yh, err);
-      
-      // calculate local error as norm of error estimate vector
-      sum = 0;
-      for (int i = 0; i < n; ++i) {
-         sum += err[i]*err[i];
-      }  
-      double e = sqrt(sum);
+      rkstep23(f, n, x, y, h, yh, err); //maybe also test other stepper
+      s = 0;
+      for (int i = 0; i < n; ++i)
+      {
+         s += err[i]*err[i];
+      }
+      double e = sqrt(s);
       //printf("Error = %.5f\n", e);
 
-      // calculate norm of estimated y(x + h)
-      sum = 0;
+      s = 0;
       for (int i = 0; i < n; ++i) {
-         sum += yh[i]*yh[i];
+         s += yh[i]*yh[i];
       }
-      double norm = sqrt(sum);
+      double norm = sqrt(s);
       //printf("Norm = %.5f\n", e);
 
-      // calculate local tolerance
-      tau = (norm*eps + acc)*sqrt(h/(b-a));
+      tol = (norm*eps + acc)*sqrt(h/(b-a));
 
-      // accept or reject step?
-      if (e < tau) {
-         k++;     // increment counter
-         x += h;  // increment x
-
-         // increment elements of y vector and print to file
+      if (e < tol)
+      {
+         k++;     
+         x += h;  
          fprintf(file, "%20.12e ", x);
          for (int i = 0; i < n; ++i) {
             y[i] = yh[i];
             fprintf(file, "%20.12e ", y[i]);
          }
          fprintf(file, "\n");
-
       }
-      
-      // adjust step size
-      if (e > 0) { h *= pow(tau/e, 0.25)*0.95; }
-      else       { h *= 2; } 
-     
-   } /* end while */
+      if (e > 0) { h *= pow(tol/e, 0.25)*0.95; }
+      else       { h *= 2; }
 
-   // set final y vector
-   for (int i = 0; i < n; ++i) {
+   } /* end while */
+   for (int i = 0; i < n; ++i)
+   {
       y[i] = yh[i];
    }
-
-   //printf("===============\n");
-   //printf("Step no. %3d\n", k);
-   //printf("x = %.4f\ny =\n", x);
-   //cvector_print(y, n);
-
-   // close output file
    fclose(file);
-
-   // return number of steps
    return k;
 
 }
-
-
-
-
-
-
-
-
-
--- Homework/Ordinary-differential-equations/main.c
+++ Homework/Ordinary-differential-equations/main.c
@@ -1,4 +1,4 @@
- 
+
 #include <assert.h>
 
 #include <gsl/gsl_matrix.h>
@@ -9,7 +9,7 @@
 
 #include "utilities.h"
 #include "Runge-Kutta.h"
-#include <stdio.h> 
+#include <stdio.h>
 
 
 void rkstep12(void f(double x,gsl_vector* y, gsl_vector* dydx), double x, gsl_vector* yx, double h, gsl_vector* yh, gsl_vector* err);
@@ -17,22 +17,22 @@ void rkstep12(void f(double x,gsl_vector* y, gsl_vector* dydx), double x, gsl_ve
 void rkstep23(void (*f)(int n, double x, double* y, double* dydx), int n, double x, double* y_curr, double h, double* y_next, double* err);
 
 int driver(
-	void (*f)(int n, double x, double* y, double* dydx), // right-hand-side of dy/dx = f(x, y) 
-   int n,          // size of vectors 
-	double  a,      // the start-point a 
-	double  b,      // the end-point of the integration 
-	double* ya,     // y(a) 
-	double* yb,     // y(b) to be calculated 
-	double  h,      // initial step-size 
-	double  acc,    // absolute accuracy goal 
+	void (*f)(int n, double x, double* y, double* dydx), 
+    int n,          // size of vectors
+	double  a,      // the start-point a
+	double  b,      // the end-point of the integration
+	double* ya,     // y(a)
+	double* yb,     // y(b) to be calculated
+	double  h,      // initial step-size
+	double  acc,    // absolute accuracy goal
 	double  eps,    // relative accuracy goal
-    char*   outfile // trajectory file
+    char*   outfile // target file
     );
 
 
 void f(int n,double x,double*y,double*dydt){
 	dydt[0]=+y[1];
-	dydt[1]=2*(x*x/2-0.5)*y[0];
+	dydt[1]=2*((x)*(x)/2-0.5)*y[0];
 	}
 
 double N;
@@ -45,10 +45,8 @@ void SIR_model(int n, double x, double* y, double*dydx) {
    dydx[2] =  y[1]/Tr;
 }
 
-
-
-int main(){
-
+int main() {
+{
     int n = 2;
     double a = 0;
     double b = 10;
@@ -59,22 +57,76 @@ int main(){
     double eps = 1e-4;
     char* outfile = "solu1.txt";
     ya[0] = 1;
-    
+
     driver(&f, n, a, b, ya, yb, h, acc, eps, outfile);
-
-   outfile = "sir1.txt";
-   N = 5.8e6;
-   Tr = 7;
-   Tc = 1.0;
-
-   ya[0] = N;
-   ya[1] = 50;
-   ya[2] = 0;
-   
-   driver(&f_SIR, n, a, b, ya, yb, h, acc, eps, outfile);
+}
 
 
+{
+    int n = 3;
+    int a = 0;
+    int b = 60;
+    double ya[n];
+    double yb[n];
+    double h = 0.05;
+    double acc = 1;
+    double eps = 1e-6;
+    char* outfile;
 
+    outfile = "sir_model_1.txt";
+    N = 5.806e6;
+    Tr = 2;
+    Tc = 1.0;
 
-return 0;    
+    ya[0] = N;
+    ya[1] = 15;
+    ya[2] = 0;
+
+    driver(&SIR_model, n, a, b, ya, yb, h, acc, eps, outfile);
+}
+{
+    int n = 3;
+    int a = 0;
+    int b = 60;
+    double ya[n];
+    double yb[n];
+    double h= 0.05;
+    double acc = 1;
+    double eps = 1e-6;
+    char* outfile;
+
+    outfile = "sir_model_2.txt";
+    N = 5.806e6;
+    Tr = 6;
+    Tc = 2;
+
+    ya[0] = N;
+    ya[1] = 15;
+    ya[2] = 0;
+
+    driver(&SIR_model, n, a, b, ya, yb, h, acc, eps, outfile);
+}
+{
+  int n = 3;
+  int a = 0;
+  int b = 60;
+  double ya[n];
+  double yb[n];
+  double h= 0.05;
+  double acc = 1;
+  double eps = 1e-6;
+  char* outfile;
+
+  outfile = "sir_model_3.txt";
+  N = 5.806e6;
+  Tr = 8;
+  Tc = 2;
+
+  ya[0] = N;
+  ya[1] = 15;
+  ya[2] = 0;
+
+  driver(&SIR_model, n, a, b, ya, yb, h, acc, eps, outfile);
+}
+   return 0;
 }
