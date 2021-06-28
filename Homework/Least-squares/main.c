3e691cdcc50084b382409148da68e9b73e002ae8
Martin Mikkelsen <MartinMikkelsen@post.au.dk>
2021-06-28 14:17:09 +0200

    least-squares corrected

--- Homework/Least-squares/main.c
+++ Homework/Least-squares/main.c
@@ -15,88 +15,94 @@ void lsfit(
         gsl_vector* x, gsl_vector* y, gsl_vector* dy,
         gsl_vector* c, gsl_matrix* S);
 
+void matrix_print(gsl_matrix* mat);
+
 int main(){
-    double x[] = {1,  2,  3, 4, 6, 9,   10,  13,  15};
+    FILE* outfile = fopen("output.txt","w");
+    double t[] = {1,  2,  3, 4, 6, 9,   10,  13,  15};
     double y[]=  {117,100,88,72,53,29.5,25.2,15.2,11.1};
-    int n=sizeof(x)/sizeof(x[0]);
+    int n=sizeof(t)/sizeof(t[0]);
     double dy[n];
-    for(int i=0;i<n;i++)dy[i]=0.05*y[i];
-
+    double ylog[n];
+    double dlogy[n];
+    for(int i=0;i<n;i++){
+		dy[i] = 0.05 * y[i];
+		dlogy[i] = dy[i]/y[i];
+		ylog[i] = log(y[i]);
+	}
+    gsl_vector* lt = gsl_vector_alloc(n);
+	gsl_vector* ly = gsl_vector_alloc(n);
+	gsl_vector* ldy = gsl_vector_alloc(n);
 	for(int i=0;i<n;i++){
-		dy[i]/=y[i];
-		y[i]=log(y[i]);
-		}
-
-	gsl_vector* vx = gsl_vector_alloc(n);
-	gsl_vector* vy = gsl_vector_alloc(n);
-	gsl_vector* vdy = gsl_vector_alloc(n);
-	for(int i=0;i<n;i++){
-		gsl_vector_set(vx,i,x[i]);
-		gsl_vector_set(vy,i,y[i]);
-		gsl_vector_set(vdy,i,dy[i]);
-		}
-
-	int m=2;
-
+		gsl_vector_set(lt,i,t[i]);
+		gsl_vector_set(ly,i,ylog[i]);
+		gsl_vector_set(ldy,i,dlogy[i]);
+	}
+    double m = 2;
+    gsl_matrix* cov = gsl_matrix_alloc(m,m);
 	gsl_vector* c = gsl_vector_alloc(m);
-	gsl_matrix* S = gsl_matrix_alloc(m,m);
-	lsfit(m,funs,vx,vy,vdy,c,S);
-
-    FILE* logdata_file = fopen("logdata.txt", "w");
-    for (int i = 0; i < n; ++i) {
-      double xi  = gsl_vector_get(vx, i);
-      double yi  = gsl_vector_get(vy, i);
-      double dyi = gsl_vector_get(vdy, i);
-      fprintf(logdata_file, "%10.5f %10.5f %10.5f\n", xi, yi, dyi);
-   }
-
-
-fprintf(stderr,"c=\n");
-for(int i=0;i<c->size;i++)fprintf(stderr,"%10g ",gsl_vector_get(c,i));
-fprintf(stderr,"\n");
-
-fprintf(stderr,"S=\n");
-for(int i=0;i<S->size1;i++){
-	for(int j=0;j<S->size2;j++)
-		fprintf(stderr,"%10g ",gsl_matrix_get(S,i,j));
-	fprintf(stderr,"\n");
+	lsfit(m, funs, lt, ly, ldy, c, cov);
+	gsl_vector* dfit  = gsl_vector_alloc(m);
+	for(int k = 0; k < m; k++){
+		gsl_vector_set(dfit, k, sqrt(gsl_matrix_get(cov, k, k)));
 	}
 
-	gsl_vector* dc = gsl_vector_alloc(m);
-	for(int k=0;k<m;k++){
-		double skk=gsl_matrix_get(S,k,k);
-		gsl_vector_set(dc,k,sqrt(skk));
-		}
-
-	double fit(double x){
-		double s=0;
-		for(int k=0;k<m;k++)s+=gsl_vector_get(c,k)*funs(k,x);
-		return s;
-		}
-
-	double fit_plus(int i, double x){
-		return fit(x)+gsl_vector_get(dc,i)*funs(i,x);
-		}
-
-	double fit_minus(int i, double x){
-		return fit(x)-gsl_vector_get(dc,i)*funs(i,x);
-		}
-
-	double c1 =gsl_vector_get(c, 1);
-	double dc1=gsl_vector_get(dc,1);
-	double T=-1/c1*log(2.0);
-	double dT=dc1/c1/c1;
-	printf("# half-life = %.3g +- %.2g days\n",T,dT);
-
-	printf("# time log(activity) delta(log(activity))\n");
-	for(int i=0;i<n;i++)printf("%g %g %g\n",x[i],y[i],dy[i]);
-	printf("\n\n");
-
-	for(int i=0;i<m;i++){
-		printf("# time fit fit_plus fit_minus; k=%i\n",i);
-		for(double z=x[0],dz=(x[n-1]-x[0])/64;z<=x[n-1];z+=dz)
-		printf("%g %g %g %g\n",z,fit(z),fit_plus(i,z),fit_minus(i,z));
-	printf("\n\n");
+    fprintf(outfile, "---------------------------\n");
+    fprintf(outfile,"The fit is given by:\n");
+	for(int i = 0; i < m; i++) {
+		fprintf(outfile, "%g*x^%i", gsl_vector_get(c, i), i);
 	}
+	fprintf(outfile, "\n\nThe value of c1 is %g.\n \n",gsl_vector_get(c, 1));
+	double t0 = -log(2)/gsl_vector_get(c, 1);
+	fprintf(outfile, "The estimation of the value for the half-life is %g days +/- %g days =  [%g %g] days\n\n",t0,t0*0.05,t0+t0*0.05,t0-t0*0.05);
+    fprintf(outfile, "The correct value is %g days. The two half-lives do not agree.\n\n", 3.6319);
+	fprintf(outfile, "The covariance matrix is\n\n");
+    fprintf(outfile, "    [");
+	for (int i = 0; i < m ; i++){
+		for(int j = 0; j < m; j++){
+		fprintf(outfile, "\t%3.g", gsl_matrix_get(cov, i, j));
+		}
+		if (i < m - 1) {
+			fprintf(outfile, "\n");
+		} else {
+			fprintf(outfile, "\t]\n");
+		}
+	}
+    fclose(outfile);
+    double fit(double x){
+		double sum =0;
+		for(int i=0;i<m;i++){
+			sum += gsl_vector_get(c, i)*funs(i, x);
+		}
+		return sum;
+	}
+
+    FILE* resultfile = fopen("fitparams.txt", "w");
+	// Data points
+	fprintf(resultfile,"# Data points\n");
+	for(int i = 0; i < n; i++) {
+		fprintf(resultfile,"%g %g %g\n", t[i], ylog[i], dlogy[i]);
+	}
+
+	fprintf(resultfile,"\n\n# Fit\n");
+	for(int i = 0; i < n; i++) {
+		fprintf(resultfile,"%g %g \n", t[i], fit(t[i]));
+	}
+
+	fprintf(resultfile,"\n\n# Fit with uncertainties= +delta(c0)+delta(c1)\n");
+	for(int i = 0; i < n; i++) {
+		fprintf(resultfile,"%g %g \n", t[i], fit(t[i]) + gsl_vector_get(dfit, 0)*funs(0, t[i]) + gsl_vector_get(dfit, 1)*funs(1, t[i]));
+	}
+
+	fprintf(resultfile,"\n\n# Fit with uncertainties = -delta(c0) - delta(c1)\n");
+	for(int i = 0; i < n; i++) {
+		fprintf(resultfile,"%g %g \n", t[i], fit(t[i]) - gsl_vector_get(dfit, 0)*funs(0, t[i]) - gsl_vector_get(dfit, 1)*funs(1, t[i]));
+	}
+
+	fclose(resultfile);
+
+	gsl_matrix_free(cov);
+	gsl_vector_free(c);
+	gsl_vector_free(dfit);
 return 0;
 }
